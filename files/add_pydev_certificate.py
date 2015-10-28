f7239ba869a9aa037b1be361afc53198a9c68266
Nathan Sowatskey <nathan@nathan.to>
2015-10-28 16:35:22 +0000

    Changing PyDev install to just copy to eclipse/plugins.

--- files/add_pydev_certificate.py
+++ files/add_pydev_certificate.py
@@ -2,9 +2,10 @@
 # Add PyDev's certificate to Java's key and certificate database
 # Certificate file here: http://pydev.org/pydev_certificate.cer
 import os, sys, pexpect, urllib2
+from pexpect import TIMEOUT
 def main():
   # NOTE: You may have to update the path to your system's cacerts file
-  certs_file = '/usr/lib/jvm/default-java/jre/lib/security/cacerts'  
+  certs_file = '/usr/lib/jvm/java-1.8.0/jre/lib/security/cacerts'  
   pydev_certs_url = 'http://pydev.org/pydev_certificate.cer'
   print "Adding pydev_certificate.cer to %s" % (certs_file)
   pydev_cert = open('pydev_certificate.cer', 'w')
@@ -14,8 +15,11 @@ def main():
   child = pexpect.spawn(cmd)
   child.expect("Enter keystore password:")
   child.sendline("changeit")
-  if child.expect(["Trust this certificate?", "already exists"]) == 0:
-    child.sendline("yes")
+  try:
+      if child.expect("Trust this certificate? [no]:") == 0:
+          child.sendline("yes")
+  except pexpect.TIMEOUT:
+      pass
   #try:
   #  child.interact()
   #except OSError:
